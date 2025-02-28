e06b5f373adff6569b739f2c6eb1447f155a0b17
Samthebest2004 <nooneknowme2004@gmail.com>
2025-02-28 08:26:04 +0530

    Describe your changes

--- test1.py
+++ test1.py
@@ -73,7 +73,7 @@ def monte_carlo_adjustment(row, weights, num_simulations=1000):
     return row
 
 # Streamlit App Title
-st.title("🧪 AI AUGMENTED CHEMICAL ANALYSIS: CHEMISTRY CALCULATOR - SAMRAT CHAKRABORTY")
+st.title("🧪 AI AUGMENTED CHEMICAL ANALYSIS: CHEMICAL CALCULATOR NATIONAL SCINECE DAY 28FEB 2025  - SAMRAT CHAKRABORTY & DEBDUTTA GHOSH")
 
 # Option selection: Default dataset or upload your own
 data_source = st.radio("Choose data source:", ("Default Dataset", "Upload your Dataset"))
